5a214612f611854b9e2682c34f5194b9a3251e0e
asdasdasd <107023302+MRX9226@users.noreply.github.com>
2026-09-03 09:20:17 +0700

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,24 @@
+import 'package:flutter/material.dart';
+import 'screens/kasir_screen.dart';
 
+void main() {
+  runApp(const KasirApp());
+}
+
+class KasirApp extends StatelessWidget {
+  const KasirApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Kasir Toko',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: const Color(0xFF233047),
+        scaffoldBackgroundColor: const Color(0xFFFBF3E4),
+        useMaterial3: true,
+      ),
+      home: const KasirScreen(),
+    );
+  }
+}
